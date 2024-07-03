073fd0be1766a521be0d05260cac5e81ebde9d04
孙栋 <sundong@idss-cn.com>
2024-07-03 20:05:10 +0800

    improvement(xlsx):功能开发测试

--- src/renderer/components/HelloWorld.vue
+++ src/renderer/components/HelloWorld.vue
@@ -3,6 +3,42 @@ import { ref } from 'vue';
 import Excel from 'exceljs'
 import fileSaver from 'file-saver'
 
+const CELL_STYLE = {
+  "font": {
+      "size": 10,
+      "color": {
+          "theme": 1
+      },
+      "name": "宋体",
+      "family": 3,
+      "charset": 134,
+      "scheme": "minor"
+  },
+  "border": {
+      "left": {
+          "style": "thin"
+      },
+      "right": {
+          "style": "thin"
+      },
+      "top": {
+          "style": "thin"
+      },
+      "bottom": {
+          "style": "thin"
+      }
+  },
+  "fill": {
+      "type": "pattern",
+      "pattern": "none"
+  },
+  "alignment": {
+      "horizontal": "center",
+      "vertical": "middle",
+      "wrapText": true
+  }
+}
+
 // 通过index下标映射英文字母
 const excelColumnName = (index) => {
     let columnName = '';
@@ -13,178 +49,186 @@ const excelColumnName = (index) => {
     }
     return columnName || undefined;
 }
-
-const selectedFile = ref(null);
-
-  const handleFileChange = (file) => {
-    selectedFile.value = file.raw;
-  };
-
-  const handleUpload = () => {
-    const reader = new FileReader();
-    reader.onload = (event) => {
-      const data = new Uint8Array(event.target.result);
-      const workbook = XLSX.read(data, { type: 'array' });
-
-      const firstSheetName = workbook.SheetNames[0];
-      const worksheet = workbook.Sheets[firstSheetName];
-      worksheet?.eachRow({ includeEmpty: true }, function(row, rowNumber) {
-      console.log('Row ' + rowNumber + ' = ' + JSON.stringify(row.values));
-        row.eachCell({ includeEmpty: true }, function(cell, colNumber) {
-          console.log('Cell ' + colNumber + ' = ' + cell.value);
-        });
-      });
-      // 将工作表内容转换为JSON
-      // const jsonData = XLSX.utils.sheet_to_json(worksheet, { header: 1 });
-
-      // // 删除第一列
-      // const modifiedData = jsonData.map(row => row.slice(1));
-
-      // // 创建一个新的工作簿和工作表
-      // const newWorkbook = XLSX.utils.book_new();
-      // const newWorksheet = XLSX.utils.aoa_to_sheet(modifiedData);
-      // XLSX.utils.book_append_sheet(newWorkbook, newWorksheet, 'Sheet1');
-
-      // // 生成新的.xlsx文件
-      // const newExcelBuffer = XLSX.write(newWorkbook, { bookType: 'xlsx', type: 'array' });
-      // const blob = new Blob([newExcelBuffer], { type: 'application/octet-stream' });
-      // const link = document.createElement('a');
-      // link.href = URL.createObjectURL(blob);
-      // link.download = 'modified.xlsx';
-      // link.click();
-    };
-    reader.readAsArrayBuffer(selectedFile.value);
-  }
-  /**
-   * 读取信息表：获取项目名称、项目编号、包号、设备名称、投标公司名称
-   */
-  const uploadFile = async (e) => {
-    const data = await e.target.files[0].arrayBuffer()
-    console.log('eeeee', data)
-    const workbook = new Excel.Workbook()
-    console.log('workbook', workbook)
-    await workbook.xlsx.load(data)
-    const worksheet = workbook.getWorksheet('投标报名信息')
-    console.log('worksheetworksheet', worksheet.getRow(1))
-    worksheet?.eachRow({ includeEmpty: true }, function(row, rowNumber) {
-    console.log('Row ' + rowNumber + ' = ' + JSON.stringify(row.values), row);
-      row.eachCell({ includeEmpty: true }, function(cell, colNumber) {
-        console.log('Cell ' + colNumber + ' = ' + cell.value, cell);
-      });
-    });
-  }
-  // 读取评分表
-  const uploadScoringFile = async (e) => {
-    const data = await e.target.files[0].arrayBuffer()
-    const workbook = new Excel.Workbook()
-    await workbook.xlsx.load(data)
-    const worksheet = workbook.getWorksheet('1')
-    worksheet?.eachRow({ includeEmpty: true }, function(row, rowNumber) {
-      const { number } = worksheet.lastRow 
-      // 最后两行不需要处理
-      if ([number, number - 1].includes(rowNumber)) {
-        return
-       }
-      handleRow(worksheet, row, rowNumber)
-
-      console.log('Row ' + rowNumber + ' = ' + JSON.stringify(row.values), row);
-      row.eachCell({ includeEmpty: true }, function(cell, colNumber) {
-        console.log('Cell ' + colNumber + ' = ' + cell.value, cell);
-      });
-    });
-    const buffer = await workbook.xlsx.writeBuffer()
+/**
+ * 读取信息表：获取项目名称、项目编号、包号、设备名称、投标公司名称
+ */
+const defaultInfo = {
+  // 项目名称
+  projectName: '',
+  // 项目编号(拼接包号)
+  projectCode: '',
+  // 包名以及关联的公司名称
+  packageData: {}
+}
+let info = ref({
+  ...defaultInfo
+})
+const uploadFile = async (e) => {
+  // 每次重新上传文件时重置
+  info = ref({
+    ...defaultInfo
+  })
+  const data = await e.target.files[0].arrayBuffer()
+  const workbook = new Excel.Workbook()
+  await workbook.xlsx.load(data)
+  const worksheet = workbook.getWorksheet('投标报名信息')
+  worksheet?.eachRow({ includeEmpty: true }, function(row, rowNumber) {
+    if (rowNumber === 1) {
+      const { projectName, projectCode } = getProjectInfo(row.values[1])
+      info.value.projectName = projectName
+      info.value.projectCode = projectCode
+    }
     
-    fileSaver(new Blob([buffer], {
-      type: 'application/octet-stream'
-    }), 'aaa.xlsx')
     
-  }
-  // 处理第一行与第二行
-  const handleRow = (worksheet, row, rowIndex = 1) => {
-    // 处理第一行与第二行需要所有单元格合并
-    const isMergeRow = [1, 2].includes(rowIndex)
-    // 第三（序号）行追加的列需要设置内容
-    const isNumberRow = rowIndex === 3
-    // 第十四行（合计）行
-    const isTotalRow = rowIndex === 14
-    // 求和开始列
-    const START_SUM_ROW = 4
-    // 获取当前列数
-    let columnCount = row.cellCount;
-    [1,2,3,4,5].forEach((item, index) => {
-      let newCell = row.getCell(columnCount + 1 + index);
-      if (isTotalRow) {
-        const lastCell = row.getCell(columnCount)
-        console.log('lastCell', lastCell, newCell)
-        // newCell.formula = `SUM()`
-        const columnName = newCell.address[0]
-        newCell.value = {
-          formula: `SUM(${columnName}${START_SUM_ROW}:${columnName}${rowIndex - 1})`,
+    if (rowNumber > 2) {
+      const packageNumber = row.values[1];
+      if (!info.value.packageData[packageNumber]) {
+        info.value.packageData[packageNumber] = {
+          deviceName: row.values[2],
+          corporateNameList: []
         }
-        // 求和校验，不超过100
+      } else {
+        info.value.packageData[row.values[1]].corporateNameList.push(row.values[4])
+      }
+      
+    }
+  });
+  console.log('info.valueinfo.value', info.value)
+}
+const getProjectInfo = (value) => {
+  const nameRegex = /.*(?=（[^（]*$)/
+  const codeRegex = /（([^（）]*)）[^（）]*$/
+  const projectName = value.match(nameRegex)[0]
+  const projectCode = value.match(codeRegex)[1]
+  console.log('matches', projectName, projectCode)
+  return {
+    projectName,
+    projectCode
+  }
+}
+// 读取评分表
+const uploadScoringFile = async (e) => {
+  const data = await e.target.files[0].arrayBuffer()
+  const workbook = new Excel.Workbook()
+  await workbook.xlsx.load(data)
+  const worksheet = workbook.getWorksheet('1')
+  
+  for (let [key, value] of Object.entries(info.value.packageData)) {
+    const newWorksheet = workbook.addWorksheet(`1-${key}`);
+    worksheet?.eachRow({ includeEmpty: true }, function(row, rowNumber) {
+      // const { number } = worksheet.lastRow 
+      row.eachCell({ includeEmpty: true }, (cell, colNumber) => {
+          const newCell = newWorksheet.getCell(rowNumber, colNumber);
+          newCell.value = cell.value;
+          if (cell.style) {
+              newCell.style = { ...cell.style };
+          }
+      });
+
+      newWorksheet?.eachRow({ includeEmpty: true }, function(newRow, newRowNumber) {
+        const { number } = newWorksheet.lastRow 
+        // 最后两行不需要处理
+        if ([number, number - 1].includes(rowNumber)) {
+          return
+        }
+        if (newRowNumber === 2) {
+          newRow.height = 30
+          newRow.alignment = { horizontal: 'left' }
+          const cell2 = newRow.getCell(1)
+          cell2.value = `项目名称：${info.value.projectName}\n项目编号：${info.value.projectCode}`
+          // cell2.style = {
+
+          // }
+        }
+        handleRow({ 
+          worksheet: newWorksheet, 
+          row: newRow, 
+          rowIndex: newRowNumber, 
+          deviceName: value.deviceName, 
+          companyList: value.corporateNameList
+        })
+      })
+    });
+  }
+  
+  workbook.removeWorksheet('1')
+  // 文件下载
+  downloadFile(workbook)
+  
+}
+// 处理行以及单元格
+const handleRow = ({ worksheet, row, rowIndex = 1, companyList, deviceName } = {} ) => {
+  
+  // 设置设备名称
+  if (rowIndex === 1) {
+    row.getCell(1).value = `${deviceName} 技术评分表`
+  }
+  // 第三（序号）行追加的列需要设置内容
+  const isNumberRow = rowIndex === 3
+  // 第十四行（合计）行
+  const isTotalRow = rowIndex === 14
+  // 求和开始列
+  const START_SUM_ROW = 4
+  // 获取当前列数
+  let columnCount = row.cellCount;
+  const lastCell = row.getCell(columnCount);
+  companyList?.forEach((item, index) => {
+    let newCell = row.getCell(columnCount + 1 + index);
+    if (isTotalRow) {
+      const columnName = newCell.address[0]
+      newCell.value = {
+        formula: `SUM(${columnName}${START_SUM_ROW}:${columnName}${rowIndex - 1})`
+      }
+      // newCell.result = 0
+    } else {
+      if (isNumberRow) {
+        newCell.value = item
+      } else {
+        newCell.value = null
+        const grade = typeof lastCell.value === 'number' ?  lastCell.value : lastCell.value?.split('-')[1]
+        // 单元格分数校验
         newCell.dataValidation = {
           allowBlank: true,
-          formulae: [6],
+          formulae: [Number(grade)],
           operator: "lessThanOrEqual",
           showErrorMessage: true,
           showInputMessage: true,
           type: 'whole'
         }
-      } else {
-        newCell.value = isNumberRow ? item : ''
       }
-      newCell.alignment = {
-        horizontal: 'center',
-        vertical: 'middle'
-      }
-      
-    })
-    if (!isMergeRow) {
-      return
     }
-    // 只有第一行与第二行需要整体单元格合并
-    const newColumnCount = row.cellCount
-    worksheet.unMergeCells(`A${rowIndex}`)
-    worksheet.mergeCells(rowIndex, newColumnCount ,rowIndex, 1)
-  }
-  // 处理第三行
-  const handleThreeRow = (worksheet) => {
-    
-  }
-  const downloadFile = async () => {
-    const workbook = new Excel.Workbook()
-    const worksheet = workbook.addWorksheet('1')
-    const buffer = await workbook.xlsx.writeBuffer()
-    
-    fileSaver(new Blob([buffer], {
-      type: 'application/octet-stream'
-    }), 'aaa.xlsx')
-  }
-  const submitUpload = () => {
-    uploadRef.value.submit();
-  };
+    // 设置单元格样式
+    newCell.style = CELL_STYLE
+    // 设置单元格宽度
+    newCell._column.width = 20
 
-  const uploadRef = ref(null);
+    if (rowIndex === 5) {
+      worksheet.mergeCells(rowIndex, columnCount + 1 + index , rowIndex - 1, columnCount + 1 + index)
+    }
+  })
+
+  // 处理第一行与第二行需要所有单元格合并
+  const isMergeRow = [1, 2].includes(rowIndex)
+  if (!isMergeRow) {
+    return
+  }
+  // 只有第一行与第二行需要整体单元格合并
+  const newColumnCount = row.cellCount
+  worksheet.unMergeCells(`A${rowIndex}`)
+  worksheet.mergeCells(rowIndex, newColumnCount ,rowIndex, 1)
+}
+// 文件导出
+const downloadFile = async (workbook) => {
+  const buffer = await workbook.xlsx.writeBuffer()
+  fileSaver(new Blob([buffer], {
+    type: 'application/octet-stream'
+  }), 'aaa.xlsx')
+}
 </script>
 
 <template>
 <div>
   请选择信息表：<input type="file" @change="uploadFile">
   请选择技术评分表：<input type="file" @change="uploadScoringFile">
-  <!-- <el-upload
-    ref="uploadRef"
-    action=""
-    :http-request="handleUpload"
-    :on-change="handleFileChange"
-  >
-    <el-button slot="trigger" size="small" type="primary">选取文件</el-button>
-    <el-button
-      style="margin-left: 10px;"
-      type="success"
-      @click="submitUpload"
-    >
-      上传并处理文件
-    </el-button>
-  </el-upload> -->
 </div>
 </template>
